adc1044df3a4731b70a5d94d542d8fab12c8946c
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2023-05-08 13:48:30 +0400

    Correction tableaux

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,50 @@
-const { BrowserWindow, Notification} = require("electron");
+const { BrowserWindow, Notification, shell } = require("electron");
 const { getConnection } = require("./database")
 
 let window;
 
-const ajout = async(stockage) => {
+const ajout = async (stockage) => {
     const connect = await getConnection();
     const result = await connect.query("INSERT INTO stockage SET ?", stockage)
 }
 
-const liste  = async() => {
+const liste = async () => {
     const connect = await getConnection();
     const result = await connect.query("SELECT * FROM stockage ORDER BY id DESC");
-    return(result);
+    return (result);
 }
-const allType  = async() => {
+const allType = async () => {
     const connect = await getConnection();
     const result = await connect.query("SELECT type_piece FROM stockage");
-    return(result);
+    return (result);
 }
 
-const getbyId  = async(id) => {
+const getbyId = async (id) => {
     const connect = await getConnection();
     const result = await connect.query("SELECT * FROM stockage WHERE id=?", id);
-    return(result[0]);
+    return (result[0]);
 }
 
-
-const singleType  = async(type) => {
+const singleType = async (type) => {
     const connect = await getConnection();
     const result = await connect.query("SELECT * FROM stockage WHERE type_piece=?", type);
-    return(result);
+    return (result);
 }
 
-const modifier  = async(id,stockage) => {
+const modifier = async (id, stockage) => {
     const connect = await getConnection();
-    const result = await connect.query("UPDATE stockage SET ? WHERE id=?",[stockage, id,]);
-    return(result);
+    const result = await connect.query("UPDATE stockage SET ? WHERE id=?", [stockage, id,]);
+    return (result);
 }
 
-const supprimer  = async(id) => {
+const supprimer = async (id) => {
     const connect = await getConnection();
     const result = await connect.query("DELETE FROM stockage WHERE id=?", id);
-    return(result);
+    return (result);
+}
+
+const mailto = async (text) => {
+    shell.openExternal("mailto:" + text)
 }
 
 function NewWindow() {
