0f04aa42a2e63ab34ef8aefb5edccdb6706e4bc2
VICTORINE Julien <julien.victorine440@gmail.com>
2023-05-10 13:31:36 +0400

    modification & ajout modal

--- src/js/database.js
+++ src/js/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: `electron_ifr`,
+    database: `electron_rep`,
     port: 5697
 })
 
